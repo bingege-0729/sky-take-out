afec7e84b3bea8e87bf45f17080e82a86b7b8cdb
Anonymous <2509094405@qq.com>
2025-09-22 20:28:46 +0800

    公共字段自动填充业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,11 +1,14 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface EmployeeMapper {
@@ -29,6 +32,8 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user) " +
             "values"+
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    //加入
+    @AutoFill(value= OperationType.INSERT)
     void insert(Employee employee);
 
     //分页查询
@@ -37,6 +42,7 @@ public interface EmployeeMapper {
 
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
     //扩展方法，动态扩展属性
+    @AutoFill(value= OperationType.UPDATE)
     void update(Employee employee);
 
     //根据id查询员工信息
